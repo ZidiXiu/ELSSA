d7ed2e01ee43aef9675ece763da73e60803aa965
zidixiu <zx35@duke.edu>
2021-06-08 11:35:38 -0400

    survival part finished and tested

--- utils/metrics.py
+++ utils/metrics.py
@@ -39,7 +39,7 @@ def get_CI_raw(event, true_t, pred_t, torch_object=False):
         return concordance_index_censored(event.astype(bool), true_t, -pred_t)
 
 # calculate point estimation loss
-def point_loss(t_hat, y, e, loss_type='MSE'):
+def point_loss(t_hat, y, e, loss_type='MSE', return_sum = True):
     # point estimation loss based on the predicted raw probabilities
     hinge_loss,_ = torch.min(torch.cat([t_hat.unsqueeze(1)-y.unsqueeze(1), torch.zeros_like(t_hat.unsqueeze(1)).to(y.device)], dim=1), 1, keepdim=True)
     
@@ -51,10 +51,14 @@ def point_loss(t_hat, y, e, loss_type='MSE'):
         eloss = (e*torch.abs(y-t_hat.squeeze())).sum()
         closs = ((1-e)*torch.abs(hinge_loss.squeeze())).sum()
     elif loss_type == 'RAE':
-        eloss = (e*torch.abs(y-t_hat.squeeze())/t_hat.squeeze()).sum()
-        closs = ((1-e)*torch.abs(hinge_loss.squeeze())/t_hat.squeeze()).sum()
+        eloss = (e*torch.abs(y-t_hat.squeeze())/y.squeeze()).sum()
+        closs = ((1-e)*torch.abs(hinge_loss.squeeze())/y.squeeze()).sum()
         
-    return eloss, closs
+    if return_sum:
+        return eloss, closs
+    else:
+        len_e = len(e[e==1])
+        return eloss/len_e, closs/(len(e)-len_e)
 
 '''time dependent Concordance Index'''
 
@@ -79,3 +83,48 @@ def pair_Ctd_DLN(t, e, test_pred_prob, tt):
     subj_j = np.random.choice(torch.where(t>t[subj_i])[0],1)
         
     return(I_Ctd_DLN(t, e, test_pred_prob, tt, subj_i,subj_j))
+
+# for the coverage plot
+def calculate_quantiles(post_prob, tt, percentiles):
+    post_prob_sum = np.cumsum(post_prob)
+    try:
+        tt_p = [tt[np.argmin(np.abs(post_prob_sum-p))] for p in percentiles]
+    except TypeError:
+        tt_p = tt[np.argmin(np.abs(post_prob_sum-percentiles))]
+        tt_p = [tt_p]
+        
+    return(np.array(tt_p))
+
+
+def calculate_coverage(pred_prob, tt, t_,quantiles, aft_model=False):
+    if not aft_model:
+        ci_list = [calculate_quantiles(post_prob,tt,quantiles) for post_prob in pred_prob]
+        ci_list = np.array(ci_list)
+    if aft_model:
+        ci0 = aft.predict_percentile(pred_prob, p=quantiles[0]).values 
+        ci1 = aft.predict_percentile(pred_prob, p=quantiles[1]).values
+        ci_list = np.concatenate([ci1, ci0], axis=1)
+        
+    coverage_list = []
+    for i in np.arange(len(t_)):
+        if (t_[i]>=ci_list[i,0]) and (t_[i]<=ci_list[i,1]):
+            coverage_list.append(1)            
+        else:
+            coverage_list.append(0)
+    return(coverage_list)
+
+def calculate_coverage_censor(pred_prob, tt, t_,quantiles, aft_model=False):
+    if not aft_model:
+        ci_list = [calculate_quantiles(post_prob,tt,quantiles) for post_prob in pred_prob]
+        ci_list = np.array(ci_list)
+    if aft_model:
+        ci0 = aft.predict_percentile(pred_prob, p=quantiles).values 
+        ci_list = np.array(ci0).reshape(len(t_))
+    coverage_list = []
+#     print(ci_list)
+    for i in np.arange(len(t_)):
+        if (t_[i]<=ci_list[i]):
+            coverage_list.append(1)            
+        else:
+            coverage_list.append(0)
+    return(coverage_list)
